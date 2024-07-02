0cac8138ac9b56a987e015134401a6c78a11d9c9
salvo1992 <145794449+salvo1992@users.noreply.github.com>
2024-07-02 19:40:38 +0200

    3

--- il-bim/src/component/hooks/useSession.js
+++ il-bim/src/component/hooks/useSession.js
@@ -1,6 +1,6 @@
 import { useEffect } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
-import {jwtDecode} from 'jwtDecode';
+import { jwtDecode } from "jwt-decode"; 
 
 const useSession = () => {
     const session = JSON.parse(localStorage.getItem('auth'));
